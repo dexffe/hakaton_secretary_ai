124693d47167d8e15e44bd609eb6e31e2e93f74b
BoneFuusen <kotov_sergey04@mail.ru>
2024-09-07 11:34:52 +0600

    feat: add db & auth

---
+++ auth/user_model.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Integer, String, Boolean, MetaData
+from sqlalchemy.orm import mapped_column, Mapped
+
+from db.db import Base
+
+metadata = MetaData()
+
+
+class User(Base):
+    id: Mapped[int] = mapped_column(
+        Integer, primary_key=True
+    )
+    email: Mapped[str] = mapped_column(
+        String(length=320), unique=True, index=True, nullable=False
+    )
+    password: Mapped[str] = mapped_column(
+        String(length=1024), nullable=False
+    )
